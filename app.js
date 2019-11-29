0a71e24a6951e8c879d02188e0561625ef824283
NgallaJuke <ngalladiouck@gmail.com>
2019-11-29 09:13:14 +0000

    Creating and Handling Views with Router

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts')
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+
+// VIEWS EJS Middlewar
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
 
 // Routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 
+//PORT
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running at port ${PORT}`)
 );
